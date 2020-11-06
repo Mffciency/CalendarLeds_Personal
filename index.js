6235d5a4f09b3d878ccd9c44123b26469611583f
Mfficiency <63962741+Mfficiency@users.noreply.github.com>
2020-11-06 09:12:45 +0100

    final 2 calendar upload

--- index.js
+++ index.js
@@ -22,14 +22,14 @@ const calendar = google.calendar({ version: 'v3', auth: oAuth2Client })
 // set up global variables
 var hourmod = 0
 var nrLeds = 144
-var baseColor = [0,0,0] //[4,8,0]
-var hourColor = [4,2,8]
-var hour3Color = [12,16,12]
-var hour12Color = [30,30,30]
-var sleepColor = [0,0,0]
-var nowColor = [0,20,0]
-var appointmentColor = [4,0,16]
-var amColor = [8,8,0]
+var baseColor = [0, 0, 0] //[4,8,0]
+var hourColor = [4, 2, 8]
+var hour3Color = [12, 16, 12]
+var hour12Color = [30, 30, 30]
+var sleepColor = [0, 0, 0]
+var nowColor = [0, 20, 0]
+var appointmentColor = [4, 0, 16]
+var amColor = [8, 8, 0]
 var pastDiv = 4
 var datatimes = []
 var LedSequence = []
@@ -40,37 +40,47 @@ Date.prototype.addHours = function (h) {
   return this;
 }
 
-function updateVars(){
+function updateVars() {
+  var show = 1 ;
+  console.log("updating vars")
+  if (show) {
+    console.log("before : nrLeds:", nrLeds, "baseColor:", baseColor, typeof baseColor, "hourColor:", hourColor, "hour3Color:", hour3Color, "hour12Color:", hour12Color, "sleepColor:", sleepColor, "nowColor:", nowColor, "appointmentColor:", appointmentColor, "amColor:", amColor, "pastDiv:", pastDiv)
+  }
   nrLeds = process.env.nrLeds;
-  baseColor = process.env.baseColor;
-  hourColor = process.env.hourColor;
-  hour3Color = process.env.hour3Color;
-  hour12Color = process.env.hour12Color;
-  sleepColor = process.env.sleepColor;
-  nowColor = process.env.nowColor;
-  appointmentColor = process.env.appointmentColor;
-  amColor = process.env.amColor;
+  baseColor = JSON.parse(process.env.baseColor);
+  hourColor = JSON.parse(process.env.hourColor);
+  hour3Color = JSON.parse(process.env.hour3Color);
+  hour12Color = JSON.parse(process.env.hour12Color);
+  sleepColor = JSON.parse(process.env.sleepColor);
+  nowColor = JSON.parse(process.env.nowColor);
+  appointmentColor = JSON.parse(process.env.appointmentColor);
+  amColor = JSON.parse(process.env.amColor);
   pastDiv = process.env.pastDiv;
-}
-
-function print(text){
- if(showPrint){
-   console.log(text);}
+  if (show) {
+    console.log("before : nrLeds:", nrLeds, "baseColor:", baseColor, typeof baseColor, "hourColor:", hourColor, "hour3Color:", hour3Color, "hour12Color:", hour12Color, "sleepColor:", sleepColor, "nowColor:", nowColor, "appointmentColor:", appointmentColor, "amColor:", amColor, "pastDiv:", pastDiv)
+  }
 
 }
 
-async function createBlank(){
+function print(text) {
+  if (showPrint) {
+    console.log(text);
+  }
+
+}
+
+async function createBlank() {
   //empty ledsequence
   LedSequence = []
 
   // create an array with 144 unlit LEDs
   for (let index = 0; index < nrLeds; index++) {
-    LedSequence.push([0,0,0])
+    LedSequence.push([0, 0, 0])
   }
   // console.log("ledstart: ", LedSequence)
 }
 
-async function prep(calendarId) {  
+async function prep(calendarId) {
   // set start of today
   const start = new Date();
   start.setHours(0, 0, 0, 0);
@@ -79,7 +89,7 @@ async function prep(calendarId) {
   // set end of today
   const end = new Date();
   end.setHours(0, 1, 0, 0);
-  end.addHours(24+hourmod);
+  end.addHours(24 + hourmod);
 
   const times = [];
 
@@ -100,35 +110,36 @@ async function prep(calendarId) {
       const evitems = response.data.items;
 
       evitems.forEach(element => {
-        if (element.start.dateTime){
+        if (element.start.dateTime) {
           // check if starttime and endtime are in the right order
-          var startpos = timeToLedpos(calTime(element.start.dateTime),true);
+          var startpos = timeToLedpos(calTime(element.start.dateTime), true);
           var endpos = timeToLedpos(calTime(element.end.dateTime));
-          
-          if (dateCheck(element.start.dateTime)==-1){
-            startpos=0;
+
+          if (dateCheck(element.start.dateTime) == -1) {
+            startpos = 0;
           }
-          if (dateCheck(element.end.dateTime)==1){
-            endpos=nrLeds;
+          if (dateCheck(element.end.dateTime) == 1) {
+            endpos = nrLeds;
           }
 
 
-          times.push([startpos,endpos]);
-          
-          
-          console.log("item name: ",element.summary)
-          console.log("start: ",element.start.dateTime);
+          times.push([startpos, endpos]);
+
+
+          console.log("item name: ", element.summary)
+          console.log("start: ", element.start.dateTime);
           // formatTime(element.start.dateTime);
         }
       });
       datatimes = times;
       console.log(datatimes);
     },
-      function (err) { console.error("Execute error", err); 
-  });
+      function (err) {
+        console.error("Execute error", err);
+      });
 }
 
-function dateCheck (timeIn){
+function dateCheck(timeIn) {
   var date = Date.parse(timeIn);
   var start = new Date();
   start.setHours(0, 0, 0, 0);
@@ -137,11 +148,11 @@ function dateCheck (timeIn){
   // set end of today
   var end = new Date();
   end.setHours(0, 1, 0, 0);
-  end.addHours(24+hourmod);
-  if (date<start){
+  end.addHours(24 + hourmod);
+  if (date < start) {
     return -1
   }
-  else if (date>end){
+  else if (date > end) {
     return 1
   }
   else {
@@ -151,28 +162,26 @@ function dateCheck (timeIn){
 
 }
 
-function calTime (timeIn){
+function calTime(timeIn) {
   //console.log("timein: ",timeIn)
-  var hour = timeIn.substring(11,13);
+  var hour = timeIn.substring(11, 13);
   //console.log("hour: ",hour)
-  var min = timeIn.substring(14,16);
+  var min = timeIn.substring(14, 16);
   return [hour, min];
 }
 
-function timeToLedpos ([hour,min], down=false){
+function timeToLedpos([hour, min], down = false) {
   // change the time into a position on the ledstrip
   //console.log("hour: ",hour,":", min)
   var deciMin = 0;
-  if (down)
-  {
-    deciMin = Math.floor(min/10);
+  if (down) {
+    deciMin = Math.floor(min / 10);
   }
-  else
-  {
-    deciMin = Math.ceil(min/10);
+  else {
+    deciMin = Math.ceil(min / 10);
   }
-  
-  var ledPos = hour*6+deciMin;
+
+  var ledPos = hour * 6 + deciMin;
   // console.log("-----------------------")
   // console.log("info:")
   // console.log(hour, min, deciMin, ledPos);
@@ -182,56 +191,56 @@ function timeToLedpos ([hour,min], down=false){
 
 
 
-async function putAppointment (datetimesIn,color){
+async function putAppointment(datetimesIn, color) {
   // pushing the appointements to the led array
   console.log("pushing appointments:")
   // console.log("dates in:",datetimesIn,"color:",color)
   datetimesIn.forEach(element => {
     // console.log(element);
-    for (let index=element[0]; index < element[1]; index++){
-      LedSequence[index]=color;
+    for (let index = element[0]; index < element[1]; index++) {
+      LedSequence[index] = color;
       // console.log(element);
     }
   });
   // console.log(LedSequence);
 }
 
-async function overlayHours (){
+async function overlayHours() {
   // console.log("overlayingHours:")
   // console.log(LedSequence);
-  for (let pos = 0; pos < nrLeds; pos++){
+  for (let pos = 0; pos < nrLeds; pos++) {
     if (pos % 72 == 0) {
       // every 12 hour
-      LedSequence[pos]=hour12Color;
+      LedSequence[pos] = hour12Color;
     }
     else if (pos % 36 == 0) {
       // every 6 hour
-      LedSequence[pos]=hour3Color;
+      LedSequence[pos] = hour3Color;
     }
     else if (pos % 18 == 0) {
       // every 3 hour
-      LedSequence[pos]=hour3Color;
+      LedSequence[pos] = hour3Color;
     }
     else if (pos % 6 == 0) {
       // n is a multiple of 6 every hour
-      LedSequence[pos]=hourColor;
+      LedSequence[pos] = hourColor;
     }
-    else if (pos < 42){
+    else if (pos < 42) {
       // if sleeping
-      if (LedSequence[pos]!=appointmentColor && LedSequence[pos]!=amColor){
-        LedSequence[pos]=sleepColor;
+      if (LedSequence[pos] != appointmentColor && LedSequence[pos] != amColor) {
+        LedSequence[pos] = sleepColor;
       }
     }
-    else if (pos > 138){
+    else if (pos > 138) {
       // if sleeping
-      if (LedSequence[pos]!=appointmentColor && LedSequence[pos]!=amColor){
-        LedSequence[pos]=sleepColor;
+      if (LedSequence[pos] != appointmentColor && LedSequence[pos] != amColor) {
+        LedSequence[pos] = sleepColor;
       }
     }
-    else{
+    else {
       // basecolor
-      if (LedSequence[pos]!=appointmentColor && LedSequence[pos]!=amColor){
-        LedSequence[pos]=baseColor;
+      if (LedSequence[pos] != appointmentColor && LedSequence[pos] != amColor) {
+        LedSequence[pos] = baseColor;
       }
     }
   }
@@ -245,7 +254,7 @@ function addZero(i) {
   return i;
 }
 
-async function dimPast (){
+async function dimPast() {
   console.log("dim past");
   console.log(LedSequence);
   var now = new Date();
@@ -254,22 +263,22 @@ async function dimPast (){
   var m = addZero(now.getMinutes());
   //console.log(now.getHours(),now.getMinutes());
   console.log(now);
-  const curPos = timeToLedpos([h,m],true);
-  
-  for (var pos = 0; pos <= curPos; pos++){
-    if (pos == curPos ) {
-      LedSequence[pos]=nowColor;
+  const curPos = timeToLedpos([h, m], true);
+
+  for (var pos = 0; pos <= curPos; pos++) {
+    if (pos == curPos) {
+      LedSequence[pos] = nowColor;
     }
-    else{
+    else {
       // console.log("ledpos", [pos]);
       // console.log("before:",LedSequence[pos]);
       //let newvar = [0,0,0]
       //console.log("newvar:",newvar);
-      let firstchar = Math.round((LedSequence[pos][0])/pastDiv);
-      console.log(firstchar);
-      let newvar = [Math.round((LedSequence[pos][0])/pastDiv),Math.round((LedSequence[pos][1])/pastDiv),Math.round((LedSequence[pos][2])/pastDiv)];
+      let firstchar = Math.round((LedSequence[pos][0]) / pastDiv);
+      console.log("ledpos:", LedSequence[pos][0], "div:", pastDiv, "firstchar:", firstchar);
+      let newvar = [Math.round((LedSequence[pos][0]) / pastDiv), Math.round((LedSequence[pos][1]) / pastDiv), Math.round((LedSequence[pos][2]) / pastDiv)];
       // console.log("newvar:",newvar);
-      LedSequence[pos]=newvar;
+      LedSequence[pos] = newvar;
       // console.log("after: ",LedSequence[pos]);
       // console.log("");
     }
@@ -277,7 +286,7 @@ async function dimPast (){
   // console.log(LedSequence);
 }
 
-async function shiftOne (){
+async function shiftOne() {
   var last = hour12Color;
   LedSequence.shift();
   LedSequence.push(last);
@@ -286,67 +295,67 @@ async function shiftOne (){
 
 async function getLeds1(hourshift) {
   hourmod = hourshift;
-  if (hourshift !=0){updateVars();}
+  if (hourshift != 0) { updateVars(); }
   // first create a blank array
   await createBlank();
   // then get the data
   await prep(calId);
-  await putAppointment(datatimes,appointmentColor);
+  await putAppointment(datatimes, appointmentColor);
   await prep(calId2);
-  await putAppointment(datatimes,amColor);
+  await putAppointment(datatimes, amColor);
   // console.log("export: ", LedSequence)
   await overlayHours();
   await dimPast();
   await shiftOne();
   console.log("ledexport: ")
   // console.log("export: ", LedSequence)
-  if (hourshift !=0){LedSequence.forEach(e => console.log(e))}
-  
-  return{LedSequence}
+  if (hourshift != 0) { LedSequence.forEach(e => console.log(e)) }
+
+  return { LedSequence }
 }
 
 
 // getLeds1(0)
-exports.getLeds1 = async function(req, res){
-  let nrLeds = req.query.nrLeds || req.body.nrLeds ;
-  let baseColor = req.query.baseColor || req.body.baseColor ;
-  let hourColor = req.query.hourColor || req.body.hourColor ;
-  let hour3Color = req.query.hour3Color || req.body.hour3Color ;
-  let hour12Color = req.query.hour12Color || req.body.hour12Color ;
-  let sleepColor = req.query.sleepColor || req.body.sleepColor ;
-  let nowColor = req.query.nowColor || req.body.nowColor ;
-  let appointmentColor = req.query.appointmentColor || req.body.appointmentColor ;
-  let amColor = req.query.amColor || req.body.amColor ;
-  let pastDiv = req.query.pastDiv || req.body.pastDiv ;
-  if (nrLeds){
-    process.env.nrLeds=nrLeds;
+exports.getLeds1 = async function (req, res) {
+  let nrLeds = req.query.nrLeds || req.body.nrLeds;
+  let baseColor = req.query.baseColor || req.body.baseColor;
+  let hourColor = req.query.hourColor || req.body.hourColor;
+  let hour3Color = req.query.hour3Color || req.body.hour3Color;
+  let hour12Color = req.query.hour12Color || req.body.hour12Color;
+  let sleepColor = req.query.sleepColor || req.body.sleepColor;
+  let nowColor = req.query.nowColor || req.body.nowColor;
+  let appointmentColor = req.query.appointmentColor || req.body.appointmentColor;
+  let amColor = req.query.amColor || req.body.amColor;
+  let pastDiv = req.query.pastDiv || req.body.pastDiv;
+  if (nrLeds) {
+    process.env.nrLeds = nrLeds;
   }
-  if (baseColor){
-    process.env.baseColor=baseColor;
+  if (baseColor) {
+    process.env.baseColor = baseColor;
   }
-  if (hourColor){
-    process.env.hourColor=hourColor;
+  if (hourColor) {
+    process.env.hourColor = hourColor;
   }
-  if (hour3Color){
-    process.env.hour3Color=hour3Color;
+  if (hour3Color) {
+    process.env.hour3Color = hour3Color;
   }
-  if (hour12Color){
-    process.env.hour12Color=hour12Color;
+  if (hour12Color) {
+    process.env.hour12Color = hour12Color;
   }
-  if (sleepColor){
-    process.env.sleepColor=sleepColor;
+  if (sleepColor) {
+    process.env.sleepColor = sleepColor;
   }
-  if (nowColor){
-    process.env.nowColor=nowColor;
+  if (nowColor) {
+    process.env.nowColor = nowColor;
   }
-  if (appointmentColor){
-    process.env.appointmentColor=appointmentColor;
+  if (appointmentColor) {
+    process.env.appointmentColor = appointmentColor;
   }
-  if (amColor){
-    process.env.amColor=amColor;
+  if (amColor) {
+    process.env.amColor = amColor;
   }
-  if (pastDiv){
-    process.env.pastDiv=pastDiv;
+  if (pastDiv) {
+    process.env.pastDiv = pastDiv;
   }
   res.status(200).send(await getLeds1(-1));
 }
